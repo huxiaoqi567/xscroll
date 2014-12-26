f87ac78784bb9e6a784768b55ff7209e3bc584b4
伯才 <xiaoqi.huxq@alibaba-inc.com>
2014-12-26 11:12:56 +0800

    plan

--- README.md
+++ README.md
@@ -1,69 +1,95 @@
-# XScroll 3.0
 
-#### 主要分核心、手势、插件、应用四部分。
+##xscroll 3.0 升级计划
 
-* 核心：xscroll.core，主要产出一个稳定的scroll容器。
+###概要：
 
-* 手势：单独拆分XGesture项目去做，完全无限逼近IOS Native去做。
+#####手势 gesture
+直接基于目前成熟的hammer.js，包含 Pan(平移)、Pinch(缩放)、Press(按下)、Rotate(旋转)、Swipe(轻滑)、Tap(点触)。
+hammerjs http://hammerjs.github.io/
 
-* 插件：更完整的插件机制，将无尽滚动(infinite)、分页器(pagination) 、侧滑编辑(swipe-editable)等作为插件处理。
+#####核心 core
+拆分core-origin/core-simulate两个模块，分别处理原生和模拟滚动方式，支持无缝切换。该种切换支持手动、自动两种方式，且部分功能 （如 scale） 可能会不可用。
 
-* 应用：基于xscroll进行高级封装，使得更方便的进行业务场景使用。
+#####工具类 util
+常用工具方法封装。
+
+#####基类 base 
+类似于kissy.base 支持自定义事件(event)、插件(plugin)可拔插机制。event可参考
+Backbone.Events
+
+#####核心组件 components
+将常用的非核心部分组件，作为components的方式集成至core。
+
+#####插件 plugins
+可拔插、按需加载的插件体系。
+
+如：
+
+pulldown 下拉刷新
+
+pullup 上拉加载
+
+infinite 无尽列表
+
+snap 截断式滚动
+
+zoom 缩放
+
+multiview 多个滚动嵌套控制
+
+xlist变更：
+
+插件化infinite，拓展xscroll原型链关于数据操作的接口。
+
+计划安排
+
+core优先进行开发，主要通过xscroll对原生和模拟滚动的进行接口统一。
+util   
+
+    base 基类
+    
+event
+
+    plugin 
+    
+hammer 手势接入 pan tap
+
+core-simulate   模拟滚动
+
+animate
+timer
+bezier
+core-origin 原生滚动
+plugin开发
+scrollbar
+pull 上下左右的Pull都可以基于这个类开发
+pulldown  继承pull
+pullup  继承pull
+multiview 多个滚动嵌套
+applications上层应用型组件
+tab
+slider
+tablist
+sliderview
+navigationview
+indicatorview
+官网建设
+首页
+demo
+文档
 
 
-#### 计划：
-
-[xscroll3.0Gantt.html](http://gitlab.alibaba-inc.com/xiaoqi.huxq/xscroll/raw/master/xscroll3.0%E8%AE%A1%E5%88%92%E8%A1%A8/xscroll3.0Gantt.html)
-
-
-* xgesture
-
-
-  * core
-     * pan
-     * pinch
-     * rotate
-     * swipe
-
-
-* xscroll
-
-  * core
-     * core
-     * timer
-     * bezier
-     * base
-     * util
-     * boundry
-     * easing
-
-  * plugin
-     * scrollbar
-     * pulldown
-     * pullup
-     * swipeedit
-     * infinite
-     * dataset
-     * pagination
-     * crousel(slider)
-     * snap
-     * indicator
-     * navigation
-     * lazyload
-     * network
-     * fastscroll(scrollaccelerate)
-
-
-* 官网建设
-
-  * 首页
-  * demo页
 
 
 
 
 
 
-![结构图](http://gtms01.alicdn.com/tps/i1/TB1gJm_GVXXXXc6aXXXAPE_KVXX-1344-1046.png_600x600.jpg)
+
+
+
+
+
+![结构图](http://gtms04.alicdn.com/tps/i4/TB13LoRGVXXXXXmXpXX7yy27VXX-1414-1128.png_600x600.jpg)
 
 
---
+++ test/hammer/xscroll-hammer-patch.js
@@ -0,0 +1,216 @@
+;(function(){ XScroll.prototype.panEndHandler= function (e) {
+      var self = this;
+      var userConfig = self.userConfig;
+      e.velocityX = -(Math.abs(e.velocityX) < 0.35 ? 0 : e.velocityX)
+       e.velocityY = -(Math.abs(e.velocityY) < 0.35 ? 0 : e.velocityY)
+      var transX = self._bounce('x', e.velocityX);
+      var transY = self._bounce('y', e.velocityY);
+      var x = transX ? transX.offset : 0;
+      var y = transY ? transY.offset : 0;
+      var duration;
+      if (transX && transY && transX.status && transY.status && transX.duration && transY.duration) {
+        //ensure the same duration
+        duration = Math.max(transX.duration, transY.duration);
+      }
+      if (transX) {
+        self.scrollX(x, duration || transX.duration, transX.easing, function (e) {
+          self._scrollEndHandler('x');
+        });
+      }
+      if (transY) {
+        self.scrollY(y, duration || transY.duration, transY.easing, function (e) {
+          self._scrollEndHandler('y');
+        });
+      }
+      //judge the direction
+      self.directionX = e.velocityX < 0 ? 'left' : 'right';
+      self.directionY = e.velocityY < 0 ? 'up' : 'down';
+    }
+
+    XScroll.prototype._bindEvt =  function () {
+      var self = this;
+      if (self.__isEvtBind)
+        return;
+      var PAN_RATE = 0.36;
+      self.__isEvtBind = true;
+      var renderTo = self.renderTo;
+      var container = self.container;
+      var content = self.content;
+      var containerWidth = self.containerWidth;
+      var containerHeight = self.containerHeight;
+      var offset = {
+        x: 0,
+        y: 0
+      };
+      var boundry = self.boundry;
+      var mc = new Hammer.Manager(renderTo);
+      var pinch = new Hammer.Pinch();
+      var pan = new Hammer.Pan();
+      var tap = new Hammer.Tap();
+      mc.add([tap,pan,pinch]);
+      mc.on("panstart", function(e) {
+        console.log(e)
+        self._prevSpeed = 0;
+        offset = self.getOffset();
+        self.translate(offset);
+        self._firePanStart(XScroll.Util.mix(e, { offset: offset }));
+      });
+
+      mc.on("pan", function(e) {
+        var posY = self.userConfig.lockY ? Number(offset.y) : Number(offset.y) + e.deltaY;
+        var posX = self.userConfig.lockX ? Number(offset.x) : Number(offset.x) + e.deltaX;
+        containerWidth = self.containerWidth;
+        containerHeight = self.containerHeight;
+        if (posY > boundry.top) {
+          //overtop 
+          posY = (posY - boundry.top) * PAN_RATE + boundry.top;
+        }
+        if (posY < boundry.bottom - containerHeight) {
+          //overbottom 
+          posY = posY + (boundry.bottom - containerHeight - posY) * PAN_RATE;
+        }
+        if (posX > boundry.left) {
+          //overleft
+          posX = (posX - boundry.left) * PAN_RATE + boundry.left;
+        }
+        if (posX < boundry.right - containerWidth) {
+          //overright
+          posX = posX + (boundry.right - containerWidth - posX) * PAN_RATE;
+        }
+        self.translate({
+          x: posX,
+          y: posY
+        });
+        self._noTransition();
+        self.isScrollingX = false;
+        self.isScrollingY = false;
+        //pan trigger the opposite direction
+        self.directionX = e.type == 'panleft' ? 'right' : e.type == 'panright' ? 'left' : '';
+        self.directionY = e.type == 'panup' ? 'bottom' : e.type == 'pandown' ? 'top' : '';
+        self._firePan(XScroll.Util.mix(e, {
+          offset: {
+            x: posX,
+            y: posY
+          },
+          directionX: e.directionX,
+          directionY: e.directionY,
+          triggerType: "pan"
+        }));
+        self.fire("scroll", XScroll.Util.mix(e, {
+          offset: {
+            x: posX,
+            y: posY
+          },
+          directionX: self.directionX,
+          directionY: self.directionY,
+          triggerType: "pan"
+        }));
+
+      });
+
+        mc.on("panend",function(e){
+            self.panEndHandler(e);
+            self._firePanEnd(e);
+        })
+
+
+        if (self.userConfig.useTransition) {
+            container.addEventListener("webkitTransitionEnd",function(e){
+              if (e.target == content && !self.isScaling) {
+              self.__scrollEndCallbackX && self.__scrollEndCallbackX({ type: 'x' });
+            }
+            if (e.target == container && !self.isScaling) {
+              self.__scrollEndCallbackY && self.__scrollEndCallbackY({ type: 'y' });
+            }
+            },false)
+        }
+
+
+      document.addEventListener("touchstart",function(e){
+        e.preventDefault();
+        self._fireTouchStart(e);
+        if (self.isScrollingX || self.isScrollingY) {
+          self.stop();
+        }
+      },false)
+
+     
+      
+      //scalable
+      if (self.userConfig.scalable) {
+        var originX, originY;
+        //init pinch gesture
+        mc.on("pinchstart",function(e){
+            scale = self.scale;
+            originX = (e.center.x - self.x) / self.containerWidth;
+            originY = (e.center.y - self.y) / self.containerHeight;
+            self.fire("pinchstart", {
+              scale: scale,
+              origin: {
+                x: originX,
+                y: originY
+              }
+            });
+        })
+
+
+        mc.on("pinch",function(e){
+          var __scale = scale * e.scale;
+          if (__scale <= self.userConfig.minScale) {
+            // s = 1/2 * a * 2^(s/a)
+            __scale = 0.5 * self.userConfig.minScale * Math.pow(2, __scale / self.userConfig.minScale);
+          }
+          if (__scale >= self.userConfig.maxScale) {
+            // s = 2 * a * 1/2^(a/s)
+            __scale = 2 * self.userConfig.maxScale * Math.pow(0.5, self.userConfig.maxScale / __scale);
+          }
+          self._scale(__scale, originX, originY);
+          self.fire("pinch", {
+            scale: __scale,
+            origin: {
+              x: originX,
+              y: originY
+            }
+          });
+        })
+
+
+        mc.on("pinchend",function(e){
+          var SCALE_TO_DURATION = 300;
+          self.isScaling = false;
+          if (self.scale < self.minScale) {
+            self.scaleTo(self.minScale, originX, originY, SCALE_TO_DURATION);
+          } else if (self.scale > self.maxScale) {
+            self.scaleTo(self.maxScale, originX, originY, SCALE_TO_DURATION);
+          }
+          self.fire("pinchend", {
+            scale: scale,
+            origin: {
+              x: originX,
+              y: originY
+            }
+          });
+        })
+
+        mc.on("tap",function(e){
+          if(e.tapCount == 1){
+               if (!self.isScrollingX && !self.isScrollingY) {
+                // simulateMouseEvent(e, 'click');
+                self._fireClick('click', e);
+              } else {
+                self.isScrollingX = false;
+                self.isScrollingY = false;
+              }
+          }
+
+          if(e.tapCount == 2){
+               originX = (e.center.x - self.x) / self.containerWidth;
+              originY = (e.center.y - self.y) / self.containerHeight;
+              self.scale > self.minScale ? self.scaleTo(self.minScale, originX, originY, 200) : self.scaleTo(self.maxScale, originX, originY, 200);
+          }
+        })
+
+
+      }
+    }
+  })()
